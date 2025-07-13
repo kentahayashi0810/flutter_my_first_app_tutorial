016d457881741ad2c5bf9af23f719224e806c110
kentahayashi0810 <kenta0810hayashi@gmail.com>
2025-07-13 19:16:59 +0900

    Finished up favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -150,6 +150,62 @@ class GeneratorPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favoriteWordList = appState.favorites.toList();
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayLarge!.copyWith(
+      color: theme.colorScheme.primary
+    );
+
+    if(appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(padding: EdgeInsets.all(20),
+        child: Text('You have ${appState.favorites.length} favorites:'),
+        ),
+        for(var pair in appState.favorites.toList())
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          )
+      ],
+    );
+
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        width: 500,
+        height: 500,
+        child: Column(
+          children: [
+            Text("My Favorite Words", style: style,),
+            Expanded(
+              child: ListView.builder(
+                itemCount: favoriteWordList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(Icons.favorite),
+                    title: Text(favoriteWordList[index].asPascalCase),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
